33559063a4ea9bde98cbaf543a740995bd1d78b5
Joshua Kraft <nathan.joshua.kraft@gmx.de>
2020-09-18 20:45:56 +0200

    Cleaning target data (transformed questions)

---
+++ src/file_len.py
@@ -0,0 +1,6 @@
+def file_len(file):
+    with open(file) as f:
+        for i, l in enumerate(f):
+            pass
+    f.close()
+    return i + 1
--- src/read_jsonl.py
+++ src/read_jsonl.py
@@ -1,16 +1,41 @@
 import jsonlines
-import ast
+from tqdm import tqdm
 
-i = 0
+from src.parse_python import parse_code_string
+from src.file_len import file_len
 
-with jsonlines.open('../res/conala-corpus/conala-mined.jsonl') as f:
-    for line in f.iter():
-        snip = line['snippet']
-        node = ast.parse(snip, mode='exec')
-        compiled = compile(node, '<sting>', mode='exec')
-        print(ast.dump(node))
-        print('\n _______________________\n')
+total_lines = file_len('../res/conala-corpus/conala-mined.jsonl')
+substrings = ['!', '@', '#', '$', ':', ',', '?', '/', ';', '.', "'", ' - ', '"', '`', '%',
+              'how can i ', 'how do i ', 'how to ', 'python: ', 'pandas: ', 'matplotlib: ',
+              'is there a way to ', 'how do you ']
 
-        if i == 10:
-            break
-        i += 1
+progress_bar = tqdm(total=total_lines, desc="Parsing Python snippets")
+
+with open("../res/created/python_small.json", "w") as outfile_0:
+
+    with open("../res/created/python_small_target.json", "w") as outfile_1:
+
+        with jsonlines.open('../res/conala-corpus/conala-mined.jsonl') as infile:
+
+            for line in infile.iter():
+                progress_bar.update(n=1)
+                snippet = line['snippet']
+                intent = line['intent']
+                try:
+                    intent = intent.lower()
+                    for substring in substrings:
+                        intent = intent.replace(substring, "")
+                    ast_line = parse_code_string(snippet)
+                except (AttributeError, TypeError, SyntaxError) as e:
+                    pass
+                try:
+                    outfile_0.write(ast_line + '\n')
+                    outfile_1.write(intent + '\n')
+                except (UnicodeEncodeError, UnicodeDecodeError):
+                    pass
+
+        infile.close()
+
+    outfile_0.close()
+
+outfile_1.close()
