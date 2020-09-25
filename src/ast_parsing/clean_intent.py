df26295d235f6a8c8c9e423a26f81166c421b47f
Joshua Kraft <nathan.joshua.kraft@gmx.de>
2020-09-25 11:54:25 +0200

    Seperated files

---
+++ src/ast_parsing/clean_intent.py
@@ -0,0 +1,10 @@
+substrings = ['!', '@', '#', '$', ':', ',', '?', '/', ';', '.', "'", ' - ', '"', '`', '%',
+              'how can i ', 'how do i ', 'how to ', 'python: ', 'pandas: ', 'matplotlib: ',
+              'is there a way to ', 'how do you ']
+
+
+def clean_intent(intent):
+    intent = intent.lower()
+    for substring in substrings:
+        intent = intent.replace(substring, "")
+    return intent
--- src/parse_main.py
+++ src/parse_main.py
@@ -2,36 +2,32 @@ import jsonlines
 from tqdm import tqdm
 
 from src.ast_parsing.parse_python import parse_code_string
+from src.ast_parsing.clean_intent import clean_intent
 from src.helper.file_len import file_len
 
 total_lines = file_len('../res/conala-corpus/conala-mined.jsonl')
-substrings = ['!', '@', '#', '$', ':', ',', '?', '/', ';', '.', "'", ' - ', '"', '`', '%',
-              'how can i ', 'how do i ', 'how to ', 'python: ', 'pandas: ', 'matplotlib: ',
-              'is there a way to ', 'how do you ']
 
 progress_bar = tqdm(total=total_lines, desc="Parsing Python snippets")
 
-with open("../res/created/python_small.json", "w") as outfile_0:
+with open("../res/created/parsed/python_small.json", "w") as outfile_0:
 
-    with open("../res/created/python_small_target.json", "w") as outfile_1:
+    with open("../res/created/parsed/python_small_target.json", "w") as outfile_1:
 
-        with jsonlines.open('../res/conala-corpus/conala-mined.jsonl') as infile:
+        with jsonlines.open('../res/dataset/conala-corpus/conala-mined.jsonl') as infile:
 
             for line in infile.iter():
                 progress_bar.update(n=1)
                 snippet = line['snippet']
                 intent = line['intent']
                 try:
-                    intent = intent.lower()
-                    for substring in substrings:
-                        intent = intent.replace(substring, "")
+                    cleaned_intent = clean_intent(intent)
                     ast_line = parse_code_string(snippet)
                 except (AttributeError, TypeError, SyntaxError) as e:
                     pass
                 try:
                     outfile_0.write(ast_line + '\n')
-                    outfile_1.write(intent + '\n')
-                except (UnicodeEncodeError, UnicodeDecodeError):
+                    outfile_1.write(cleaned_intent + '\n')
+                except (UnicodeEncodeError, UnicodeDecodeError) as e:
                     pass
 
         infile.close()
