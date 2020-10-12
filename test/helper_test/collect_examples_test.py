57e4ee7fcac72c17c507c39d56e7c6ed8c631fda
Joshua Kraft <nathan.joshua.kraft@gmx.de>
2020-10-12 19:57:00 +0200

    Added collect_example Testcase

---
+++ test/helper_test/collect_examples_test.py
@@ -0,0 +1,33 @@
+from src.helper.collect_examples import collect_examples
+from src.ast_parsing.parse_python import parse_code_string
+import unittest
+import os
+
+
+class MainFileCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.ast = parse_code_string('print("Hi")')
+        print(self.ast)
+        with open('test.json', 'w') as f:
+            for i in range(20):
+                if i % 2 == 0:
+                    f.write('target\n')
+                else:
+                    f.write(self.ast + '\n')
+        f.close()
+
+    def test_collect_examples(self):
+        examples = collect_examples('test.json')
+        self.assertEqual(len(examples), 10)
+        self.assertEqual(type(examples), list)
+        self.assertEqual(len(examples[0]), 2)
+        self.assertEqual(type(examples[0]), list)
+        self.assertEqual(examples[0][0], 'target')
+        self.assertEqual(type(examples[0][1]), list)
+
+    def tearDown(self) -> None:
+        os.remove('test.json')
+
+
+if __name__ == '__main__':
+    unittest.main()
