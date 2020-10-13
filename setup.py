aec3a4e3c7724b05cfdcb4f66e5a6e93da54a56a
Joshua Kraft <nathan.joshua.kraft@gmx.de>
2020-10-13 18:34:06 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='CoNaLa-Extractor',
+    version='1.1',
+    packages=[''],
+    url='',
+    license='',
+    author='joshuakraft',
+    author_email='nathan.joshua.kraft@gmx.de',
+    description='MLoSC 2020'
+)
